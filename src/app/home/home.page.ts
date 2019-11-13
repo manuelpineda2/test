e7f80556af375287531c67ec262ce0ec2da022a9
Manuel Pineda <manuelhumbertopineda2@gmail.com>
2019-11-13 15:21:31 -0600

    first

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {IonContent} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    @ViewChild(IonContent, {read: ElementRef, static: true}) contentArea: ElementRef;
+    private observer: IntersectionObserver;
 
-  constructor() {}
+    constructor(private renderer: Renderer2) {
+    }
 
+    ngOnInit() {
+    }
+
+    handleScroll(ev) {
+        // console.log(ev);
+        let trans = false;
+        let scrollTop;
+        let current;
+        if (ev && ev.detail && ev.detail.scrollTop) {
+            scrollTop = ev.detail.scrollTop;
+            trans = scrollTop >= 190;
+            if (scrollTop < 150) {
+                current = (200 - scrollTop);
+                console.log(current);
+                this.renderer.setStyle(this.contentArea.nativeElement, 'transform', `translate3d(0, ${current}px,0)`);
+            } else {
+                console.log('Reached 200');
+            }
+
+        }
+        // console.log(ev);
+
+        // if (trans === true) {
+        //     this.renderer.addClass(this.contentArea.nativeElement, 'no-transform');
+        // } else {
+        //     this.renderer.removeClass(this.contentArea.nativeElement, 'no-transform');
+        // }
+    }
 }
